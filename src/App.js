8e08f6f883a23fc6575150d34810d8cb584f7288
Venkat Polumahanti <venkat4541@gmail.com>
2020-06-14 06:50:13 -0400

    - Added Axios for API interactions. - Updated add, edit and view Todos components and related actions.

--- src/App.js
+++ src/App.js
@@ -10,60 +10,37 @@ import TodosList from "./components/todos-list.component";
 function App() {
   return (
     <Router>
-      <div className="">
-        <Route path="/" exact component={TodosList} />
-        <Route path="/edit/:id" component={EditTodo} />
-        <Route path="/create" component={CreateTodo} />
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="container mx-auto px-4">
         <div className="">
-          <div className="">
-            Noitac!
-          </div>
-          <p className="">
-            One stop solution for all your technical and other discussions.
-          </p>
+          <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+            <div className="flex items-center flex-shrink-0 text-white mr-6">
+              <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
+              <span className="font-semibold text-xl tracking-tight">
+                <Link to="/" className="navbar-brand">NOITAC</Link>
+              </span>
+            </div>
+            <div className="block lg:hidden">
+              <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+                <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+              </button>
+            </div>
+            <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+              <div className="text-sm lg:flex-grow">
+              </div>
+              <div>
+                  <Link to="/" className="inline-block text-sm px-4 py-2 leading-none rounded text-teal-200 hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Todos</Link>
+                  <Link to="/create" className="inline-block text-sm px-4 py-2 leading-none rounded text-teal-200 hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Create ToDo</Link>
+              </div>
+            </div>
+          </nav>
+          <br/>
+          <Route path="/" exact component={TodosList} />
+          <Route path="/edit/:id" component={EditTodo} />
+          <Route path="/create" component={CreateTodo} />
         </div>
       </div>
-
-      <div className="container">
-        <nav>
-          <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
-          <div className="collpase navbar-collapse">
-            <ul className="navbar-nav mr-auto">
-              <li className="navbar-item">
-                <Link to="/" className="nav-link">Todos</Link>
-              </li>
-              <li className="navbar-item">
-                <Link to="/create" className="nav-link">Create Todo</Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
-        <br/>
-        <Route path="/" exact component={TodosList} />
-        <Route path="/edit/:id" component={EditTodo} />
-        <Route path="/create" component={CreateTodo} />
-      </div>
     </Router>
-    
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  );
+  )
 }
 
 export default App;
--- src/components/todos-list.component.js
+++ src/components/todos-list.component.js
@@ -1,11 +1,61 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+const Todo = props => (
+  <tr>
+      <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_description}</td>
+      <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_responsible}</td>
+      <td className={props.todo.todo_completed ? 'completed' : ''}>{props.todo.todo_priority}</td>
+      <td>
+          <Link to={"/edit/"+props.todo._id}>Edit</Link>
+      </td>
+  </tr>
+)
 
 export default class TodosList extends Component {
-    render() {
-        return (
-            <div>
-                <p>Welcome to Todos List Component!!</p>
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {todos: []};
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:8080/todos/')
+      .then(response => {
+        this.setState({ todos: response.data });
+      })
+     .catch((error) => {
+        console.log(error);
+      })
+  }
+
+  todoList() {
+    return this.state.todos.map((currentTodo, i) => {
+      return <Todo todo={currentTodo} key={i} />;
+    })
+  }
+
+  updateTodo(todo) {
+    console.log(todo);
+ }
+
+  render() {
+    return (
+      <div className="px-4">
+        <h3>Todos List</h3>
+        <table className="" style={{ marginTop: 20 }} >
+          <thead>
+              <tr>
+                <th>Description</th>
+                <th>Responsible</th>
+                <th>Priority</th>
+                <th>Action</th>
+              </tr>
+          </thead>
+          <tbody>
+            { this.todoList() }
+          </tbody>
+        </table>
+      </div>
+  )}
 }
