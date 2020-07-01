bd6c892523242a03e8e644932fe59ce9b00e9843
Venkat Polumahanti <venkat4541@gmail.com>
2020-06-30 23:30:08 -0400

    Edit task now has updated priority option selected

--- src/components/edit-todo.component.js
+++ src/components/edit-todo.component.js
@@ -102,6 +102,7 @@ export default class EditTodo extends Component {
               <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Priority: </label>
                   <select 
                     onChange={this.onChangeTodoPriority}
+                    value={this.state.priority}
                     className="block appearance-none w-full lg:w-1/3 md:w-1/2 sm:w-full cursor-pointer bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                   >
                     <option value="Low">Low</option>
